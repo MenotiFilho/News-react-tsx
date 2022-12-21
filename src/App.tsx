b3d59ce377e108fb7159778f18171f78a0bc4beb
Menoti Filho <menotimfilho@gmail.com>
2022-12-21 01:56:50 -0300

    A message describing what you have done to make this snapshot different

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import "./App.css";
 
-import React, { useState } from "react";
-
-import logo from "./logo.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <p>This is a front end mentor project</p>
