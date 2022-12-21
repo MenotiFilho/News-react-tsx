397af6f4b0a645a33019c81e93a7bf93fc1506b8
Menoti Filho <menotimfilho@gmail.com>
2022-12-21 01:50:16 -0300

    Add all my files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>This is a front end mentor project</p>
+    </div>
+  );
+}
+
+export default App;
